038a1c49434f778ef3ea9360c71b9208507042e9
Brandon Yeung <acer@wifi-roaming-128-4-186-213.wifi.udel.edu>
2024-03-02 14:10:44 -0500

    homepage refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import React, {useState} from 'react'
+import {Table, Button} from 'react-bootstrap'
 
-const problems = ["problem 1", "problem 2", "problem 3"];
-const creators = ["Leo 1", "Leo 2", "Leo 3"];
-const difficulty = ["difficulty 1", "difficulty 2", "difficulty 3"];
-const leaderboard = ["button 1", "button 2", "button 3"];
+const handleButtonClick = (rowIndex) => {
+  // Handle button click for the specific row (index) here
+  console.log(`Button clicked for Row ${rowIndex + 1}`);
+};
 
 function App() {
   return (
@@ -14,50 +15,57 @@ function App() {
       <div classname="search-bar">
         <input placeholder="Problem search..."></input>
       </div>
-      <div classname="problem-name">Name of problem
-        <ul>
-          {problems.map((problem, index) => (
-            <li key={index}>{problem}</li>
-          ))}
-
-        </ul>
+      <div classname="new-problem">
+        <Button>
+          Create New Problem
+        </Button>
       </div>
-      <div classname="problem-creator">Creator of problem
-        <ul>
-          {creators.map((creator, index) => (
-            <li key={index}>{creator}</li>
-          ))}
+      <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Creator</th>
+          <th>Difficulty</th>
+          <th></th>
+          <th>Leaderboards</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Problem 1</td>
+          <td>Leo 1</td>
+          <td>6th grade</td>
+          <td>
+            <Button variant="primary" onClick={() => handleButtonClick(0)}>
+              Play
+            </Button>
+          </td>
+            <Button variant="primary" onClick={() => handleButtonClick(0)}>
+              Leaderboard
+            </Button>
+          <td>
 
-        </ul>
-      </div>
-      <div classname="problem-difficulty">Difficulty of problem
-        <ul>
-          {difficulty.map((difficulty, index) => (
-            <li key={index}>{difficulty}<button onClick={() => handleButtonClick(index)}>Play</button></li>
-          ))}
-
-        </ul>
-      </div>
-      <div classname="problem-play-button">
-        
-
-      </div>
-      <div classname="problem-leaderboard">leaderboards
-        <ul>
-          {leaderboard.map((leaderboard, index) => (
-            <li key={index}>
-              <button onClick={() => handleButtonClick(index)}>{leaderboard}</button>
-              </li>
-          ))}
-        </ul>
-      </div>
-
+          </td>
+        </tr>
+        <tr>
+          <td>Problem 2</td>
+          <td>Leo 2</td>
+          <td>6th grade</td>
+          <td>
+            <Button variant="primary" onClick={() => handleButtonClick(0)}>
+              Play
+            </Button>
+          </td>
+          <td>
+            <Button variant="primary" onClick={() => handleButtonClick(0)}>
+              Leaderboard
+            </Button>
+          </td>
+        </tr>
+      </tbody>
+    </Table>
     </div>
   );
-  const handleButtonClick = (index) => {
-    // Handle button click for the specific element (index) here
-    console.log(`Button clicked for element at index ${index}`);
-  };
 }
 
 
