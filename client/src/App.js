48ea3af9280ba51a67d698d0ea33fa047a5d3da0
Brandon Yeung <acer@wifi-roaming-128-4-186-213.wifi.udel.edu>
2024-03-02 16:55:54 -0500

    score has color

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import {Table} from 'react-bootstrap'
 
 function App() {
   return (
+    <div>
     <p>hello world!</p>
+    <div classname="results-page">
+      Results
+      <p>Congratulations you have completed the problem set!</p>
+      <Table>
+        <thead>
+          <th>Your score was:</th>
+          <th>Your time was:</th>
+        </thead>
+        <tbody>
+          <td style={{color: /*user.score*/ >= 50 ? 'green' : 'red'}}>"Score"</td>
+          <td>"Time"</td>
+        </tbody>
+      </Table>
+    </div>
+    </div>
   );
 }
 
