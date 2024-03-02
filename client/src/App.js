1f21c68cc9c48bd6b3427567e3d167d0243d9690
Brandon Yeung <acer@wifi-roaming-128-4-186-213.wifi.udel.edu>
2024-03-02 13:15:29 -0500

    homepage start XD

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import React, {useState} from 'react'
 
 function App() {
   return (
-    <p>hello world!</p>
+    <div classname="homepage">
+      <p>Welcome back user</p>
+      <text>Problem Set</text>
+      <div classname="search-bar">Search bar</div>
+      <div classname="problem-name">Name of problem</div>
+      <div classname="problem-creator">Creator of problem</div>
+      <div classname="problem-difficulty">Difficulty of problem</div>
+
+    </div>
   );
 }
+
 
 export default App;
