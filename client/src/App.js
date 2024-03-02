e6156c8b3783c1f0e1c1711fa4bd64c9f6d6d2da
Brandon Yeung <acer@wifi-roaming-128-4-186-213.wifi.udel.edu>
2024-03-02 13:38:22 -0500

    more homepage stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,57 @@
 import './App.css';
 import React, {useState} from 'react'
 
+const problems = ["problem 1", "problem 2", "problem 3"];
+const creators = ["creator 1", "creator 2", "creator 3"];
+const difficulty = ["difficulty 1", "difficulty 2", "difficulty 3"];
+
+
 function App() {
   return (
     <div classname="homepage">
       <p>Welcome back user</p>
       <text>Problem Set</text>
-      <div classname="search-bar">Search bar</div>
-      <div classname="problem-name">Name of problem</div>
-      <div classname="problem-creator">Creator of problem</div>
-      <div classname="problem-difficulty">Difficulty of problem</div>
+      <div classname="search-bar">
+        <input placeholder="Problem search..."></input>
+      </div>
+      <div classname="problem-name">Name of problem
+        <ul>
+          {problems.map((problem, index) => (
+            <li key={index}>{problem}</li>
+          ))}
+
+        </ul>
+      </div>
+      <div classname="problem-creator">Creator of problem
+        <ul>
+          {creators.map((creator, index) => (
+            <li key={index}>{creator}</li>
+          ))}
+
+        </ul>
+      </div>
+      <div classname="problem-difficulty">Difficulty of problem
+        <ul>
+          {difficulty.map((difficulty, index) => (
+            <li key={index}>{difficulty}<button onClick={() => handleButtonClick(index)}>Click me</button></li>
+          ))}
+
+        </ul>
+      </div>
+      <div classname="problem-play-button">
+        
+
+      </div>
+      <div classname="problem-leaderboard">leaderboards
+      
+      </div>
 
     </div>
   );
+  const handleButtonClick = (index) => {
+    // Handle button click for the specific element (index) here
+    console.log(`Button clicked for element at index ${index}`);
+  };
 }
 
 
