731cf2fd1a96267a53ee5681163f513062d441a0
conorjur@udel.edu <65098646+ConorJurewicz@users.noreply.github.com>
2024-03-02 16:00:22 -0500

    bs

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,82 +1,39 @@
-import "../App.css";
-import axios from "axios";
-import config from "../config";
-import "./HomePage.css";
+import "./App.css";
 import React, { useState } from "react";
-import { Table, Button } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
 
-const handleButtonClick = (rowIndex) => {
-    // Handle button click for the specific row (index) here
-    console.log(`Button clicked for Row ${rowIndex + 1}`);
-};
+import Register from "./components/Register";
+import Login from "./components/Login";
+import HomePage from "./components/HomePage";
 
-function HomePage() {
+function App() {
+    const [user, setLoginUser] = useState({});
+
     return (
-        <div classname="homepage">
-            <p>Welcome back, USERNAME!</p>
-            <text>Problem Set</text>
-            <div classname="search-bar">
-                <input placeholder="Problem search..."></input>
+        <div className="App">
+            <div className="App">
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            user && user._id ? (
+                                <HomePage currentUser={user} />
+                            ) : (
+                                <Login setLoginUser={setLoginUser} />
+                            )
+                        }
+                    />
+
+                    <Route exact path="/register" element={<Register />} />
+                    <Route
+                        exact
+                        path="/login"
+                        element={<Login setLoginUser={setLoginUser} />}
+                    />
+                </Routes>
             </div>
-            <div classname="new-problem">
-                <Button>Create New Problem</Button>
-            </div>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Creator</th>
-                        <th>Difficulty</th>
-                        <th></th>
-                        <th>Leaderboards</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Problem 1</td>
-                        <td>Leo 1</td>
-                        <td>6th grade</td>
-                        <td>
-                            <Button
-                                variant="primary"
-                                onClick={() => handleButtonClick(0)}
-                            >
-                                Play
-                            </Button>
-                        </td>
-                        <Button
-                            variant="primary"
-                            onClick={() => handleButtonClick(0)}
-                        >
-                            Leaderboard
-                        </Button>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>Problem 2</td>
-                        <td>Leo 2</td>
-                        <td>6th grade</td>
-                        <td>
-                            <Button
-                                variant="primary"
-                                onClick={() => handleButtonClick(0)}
-                            >
-                                Play
-                            </Button>
-                        </td>
-                        <td>
-                            <Button
-                                variant="primary"
-                                onClick={() => handleButtonClick(0)}
-                            >
-                                Leaderboard
-                            </Button>
-                        </td>
-                    </tr>
-                </tbody>
-            </Table>
         </div>
     );
 }
 
-export default HomePage;
+export default App;
