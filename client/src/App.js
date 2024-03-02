65538c847aac0dbe23ee33ae8ddf8baf93ed2c91
winecg <winecg@udel.edu>
2024-03-02 13:39:54 -0500

    start for submit problem page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import { SubmitProblem } from './components/SubmitProblem.js';
 
 function App() {
   return (
-    <p>hello world!</p>
+    <><p>hello world!</p><SubmitProblem></SubmitProblem></>
   );
 }
 
