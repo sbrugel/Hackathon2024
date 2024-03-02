926869b84b3ed96b9eb973c697ae23ffa273a1c6
conorjur@udel.edu <65098646+ConorJurewicz@users.noreply.github.com>
2024-03-02 15:54:42 -0500

    homepage changes

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,88 +1,82 @@
 import "../App.css";
 import axios from "axios";
 import config from "../config";
+import "./HomePage.css";
+import React, { useState } from "react";
 import { Table, Button } from "react-bootstrap";
-import React, { useState, useEffect } from "react";
 
 const handleButtonClick = (rowIndex) => {
     // Handle button click for the specific row (index) here
     console.log(`Button clicked for Row ${rowIndex + 1}`);
 };
 
-function HomePage({ currentUser }) {
-    const [problemSets, setProblemSets] = useState([]);
-    const [users, setUsers] = useState([]);
-    // ...
-    useEffect(() => {
-        axios
-            .get("http://localhost:" + config.PORT + "/problemsets")
-            .then((res) => {
-                setProblemSets(res.data);
-            });
-    }, []);
-
-    useEffect(() => {
-        axios.get("http://localhost:" + config.PORT + "/users").then((res) => {
-            setUsers(res.data);
-        });
-    }, [problemSets]);
-
-    if (!users && !problemSets) {
-        return <p>Loading...</p>;
-    } else {
-        return (
-            <div classname="homepage">
-                <h1>Welcome back, {currentUser.name}!</h1>
-                <div classname="search-bar">
-                    <input placeholder="Problem search..."></input>
-                </div>
-                <div classname="new-problem">
-                    <Button>Create New Problem Set</Button>
-                </div>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Creator</th>
-                            <th>Difficulty</th>
-                            <th></th>
-                            <th>Leaderboards</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {problemSets.map((set) => {
-                            return (
-                                <tr>
-                                    <td>{set.name}</td>
-                                    <td>
-                                        {users.find(
-                                            (user) => user.id === set.authorID
-                                        )?.name ?? "A user"}
-                                    </td>
-                                    <td>{set.category}</td>
-                                    <td>
-                                        <Button
-                                            variant="primary"
-                                            onClick={() => handleButtonClick(0)}
-                                        >
-                                            Play
-                                        </Button>
-                                    </td>
-                                    <Button
-                                        variant="primary"
-                                        onClick={() => handleButtonClick(0)}
-                                    >
-                                        Leaderboard
-                                    </Button>
-                                    <td></td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </Table>
+function HomePage() {
+    return (
+        <div classname="homepage">
+            <p>Welcome back, USERNAME!</p>
+            <text>Problem Set</text>
+            <div classname="search-bar">
+                <input placeholder="Problem search..."></input>
             </div>
-        );
-    }
+            <div classname="new-problem">
+                <Button>Create New Problem</Button>
+            </div>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Creator</th>
+                        <th>Difficulty</th>
+                        <th></th>
+                        <th>Leaderboards</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Problem 1</td>
+                        <td>Leo 1</td>
+                        <td>6th grade</td>
+                        <td>
+                            <Button
+                                variant="primary"
+                                onClick={() => handleButtonClick(0)}
+                            >
+                                Play
+                            </Button>
+                        </td>
+                        <Button
+                            variant="primary"
+                            onClick={() => handleButtonClick(0)}
+                        >
+                            Leaderboard
+                        </Button>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Problem 2</td>
+                        <td>Leo 2</td>
+                        <td>6th grade</td>
+                        <td>
+                            <Button
+                                variant="primary"
+                                onClick={() => handleButtonClick(0)}
+                            >
+                                Play
+                            </Button>
+                        </td>
+                        <td>
+                            <Button
+                                variant="primary"
+                                onClick={() => handleButtonClick(0)}
+                            >
+                                Leaderboard
+                            </Button>
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
+        </div>
+    );
 }
 
 export default HomePage;
