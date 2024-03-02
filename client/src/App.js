67ab9b8fd106d3c9ba1211e600d8c8b6daf8ad91
Brandon Yeung <acer@wifi-roaming-128-4-186-213.wifi.udel.edu>
2024-03-02 14:18:08 -0500

    removed from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,4 @@
 import './App.css';
-import React, {useState} from 'react'
-import {Table, Button} from 'react-bootstrap'
-
-const handleButtonClick = (rowIndex) => {
-  // Handle button click for the specific row (index) here
-  console.log(`Button clicked for Row ${rowIndex + 1}`);
-};
 
 function App() {
   return (
