84a1da63bbe52b95dd4fea473356f63615bb2f2a
Simon <sbrugel@udel.edu>
2024-03-03 10:32:46 -0500

    forum as complete as can be

--- client/src/components/Forum.js
+++ client/src/components/Forum.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Forum.css"; // Import the CSS file
-import { Button } from "react-bootstrap";
 import axios from "axios";
 import config from "../config"; // Import the 'config' module
+import { Modal, Button } from "react-bootstrap";
 
 const Forum = ({ currentUser }) => {
     const buttons = [
@@ -11,94 +11,246 @@ const Forum = ({ currentUser }) => {
         // Add more buttons as needed
     ];
 
-    const [activeButton, setActiveButton] = useState(null);
-    const [textBoxValues, setTextBoxValues] = useState({});
-    const [currentInput, setCurrentInput] = useState("");
+    const [forceReload, setForceReload] = useState(0);
+    const [topics, setTopics] = useState(null);
+    const [posts, setPosts] = useState(null);
+    const [users, setUsers] = useState(null);
 
-    const handleButtonClick = (buttonId) => {
-        if (activeButton === buttonId) {
-            setActiveButton(null);
-        } else {
-            setActiveButton(buttonId);
-        }
-    };
+    useEffect(() => {
+        axios
+            .get("http://localhost:" + config.PORT + "/users", {
+                headers: {
+                    "Cache-Control": "no-cache",
+                    Pragma: "no-cache",
+                    Expires: "0"
+                }
+            })
+            .then((res) => {
+                setUsers(res.data);
+            });
+    }, []);
+
+    useEffect(() => {
+        if (!users) return;
+        axios
+            .get("http://localhost:" + config.PORT + "/topics", {
+                headers: {
+                    "Cache-Control": "no-cache",
+                    Pragma: "no-cache",
+                    Expires: "0"
+                }
+            })
+            .then((res) => {
+                setTopics(res.data);
+            });
+    }, [users]);
+
+    useEffect(() => {
+        if (!topics) return;
+        axios
+            .get("http://localhost:" + config.PORT + "/posts", {
+                headers: {
+                    "Cache-Control": "no-cache",
+                    Pragma: "no-cache",
+                    Expires: "0"
+                }
+            })
+            .then((res) => {
+                setPosts((prevState) => res.data);
+            });
+    }, [topics, forceReload]);
+
+    useEffect(() => {
+        console.log(posts);
+    }, [posts]);
+
+    const [currentInput, setCurrentInput] = useState("");
+    const [textBoxValues, setTextBoxValues] = useState([]);
+    const [activeTopic, setActiveTopic] = useState(null);
 
     const handleTextBoxChange = (event) => {
         setCurrentInput(event.target.value);
     };
 
-    const handleEnterKeyPress = (event) => {
-        if (event.key === "Enter" && activeButton) {
-            event.preventDefault(); // Prevent the default behavior of the Enter key
-
-            setTextBoxValues((prevValues) => ({
-                ...prevValues,
-                [activeButton]: {
-                    messages: [
-                        ...(prevValues[activeButton]?.messages || []),
-                        currentInput
-                    ]
-                }
-            }));
-
-            // Clear the text box by resetting the current input state
-            setCurrentInput("");
-        }
-    };
-
-    return (
-        <div>
-            <h1>Welcome to the Tutoring Forum, {currentUser.name}!</h1>
-            {buttons.map((button) => (
-                <div key={button.id}>
-                    <ButtonWithTextBox
-                        buttonId={button.id}
-                        buttonText={button.text}
-                        isActive={activeButton === button.id}
-                        onClick={handleButtonClick}
-                    />
-                    {activeButton === button.id && (
-                        <TextBox
-                            messages={
-                                textBoxValues[activeButton]?.messages || []
+    if (!(users && posts && topics)) return <p>Loading...</p>;
+    else {
+        return (
+            <div>
+                <h1>Welcome to the Tutoring Forum, {currentUser.name}!</h1>
+                <Button
+                    className="new_problem"
+                    variant="primary"
+                    onClick={() =>
+                        axios.post(
+                            "http://localhost:" + config.PORT + "/topic",
+                            {
+                                originalAuthorID: currentUser.id,
+                                title: "Need Help on Algebra Set!"
                             }
-                            onChange={handleTextBoxChange}
-                            onKeyPress={handleEnterKeyPress}
-                            value={currentInput}
-                        />
-                    )}
-                </div>
-            ))}
-        </div>
-    );
+                        )
+                    }
+                >
+                    New Topic
+                </Button>
+                {activeTopic ? (
+                    <TextBox
+                        messages={activeTopic.postIDs || []}
+                        posts={posts}
+                        users={users}
+                        onChange={handleTextBoxChange}
+                        forceReload={forceReload}
+                        setForceReload={setForceReload}
+                        activeTopic={activeTopic}
+                        currentUser={currentUser}
+                        setCurrentInput={setCurrentInput}
+                        value={currentInput}
+                    />
+                ) : null}
+                {topics.map((topic) => (
+                    <div key={topic.id}>
+                        <h2>{topic.title}</h2>
+                        <p>
+                            <em>
+                                Posted by{" "}
+                                <strong>
+                                    <u>
+                                        {
+                                            users.find(
+                                                (user) =>
+                                                    user.id ===
+                                                    topic.originalAuthorID
+                                            ).name
+                                        }
+                                    </u>{" "}
+                                </strong>
+                                on{" "}
+                                {new Date(topic.postTime).toLocaleString(
+                                    "en-US",
+                                    {
+                                        month: "2-digit",
+                                        day: "2-digit",
+                                        year: "numeric",
+                                        hour: "2-digit",
+                                        minute: "2-digit"
+                                    }
+                                )}
+                            </em>
+                        </p>
+                        <Button
+                            className="new_problem"
+                            style={{
+                                backgroundColor:
+                                    activeTopic === topic ? "red" : "green",
+                                color: activeTopic === topic ? "white" : "black"
+                            }}
+                            onClick={() => {
+                                if (activeTopic === topic) {
+                                    setActiveTopic(null);
+                                } else {
+                                    setActiveTopic(topic);
+                                }
+                            }}
+                        >
+                            {activeTopic === topic ? "Close" : "Open"}
+                        </Button>
+                    </div>
+                ))}
+            </div>
+        );
+    }
 };
 
-const ButtonWithTextBox = ({ buttonId, buttonText, isActive, onClick }) => {
-    return (
-        <div>
-            <button onClick={() => onClick(buttonId)}>
-                {isActive ? "Hide " : "Show "} {buttonText}
-            </button>
-        </div>
-    );
-};
-
-const TextBox = ({ messages, onChange, onKeyPress, value }) => {
+const TextBox = ({
+    messages,
+    posts,
+    users,
+    onChange,
+    forceReload,
+    setForceReload,
+    activeTopic,
+    currentUser,
+    setCurrentInput,
+    value
+}) => {
     return (
         <div className="textbox-container">
             <div className="message-list">
-                {messages.map((message, index) => (
-                    <div key={index} className="message">
-                        {message}
-                    </div>
-                ))}
+                {messages.map((message, index) => {
+                    const myPost = posts.find((post) => post.id === message);
+                    return (
+                        <div key={index} className="message">
+                            <img
+                                src={
+                                    users.find(
+                                        (user) => user.id === myPost.authorID
+                                    )?.avatarURL
+                                }
+                                alt="Profile"
+                                style={{
+                                    width: "50px",
+                                    height: "50px",
+                                    borderRadius: "50%",
+                                    display: "inline-block"
+                                }}
+                            />
+                            <div
+                                style={{
+                                    display: "inline-block",
+                                    marginLeft: "10px"
+                                }}
+                            >
+                                <p style={{ color: "gray" }}>
+                                    {
+                                        users.find(
+                                            (user) =>
+                                                user.id === myPost.authorID
+                                        ).name
+                                    }{" "}
+                                    posted on{" "}
+                                    {new Date(myPost.postTime).toLocaleString(
+                                        "en-US",
+                                        {
+                                            month: "2-digit",
+                                            day: "2-digit",
+                                            year: "numeric",
+                                            hour: "2-digit",
+                                            minute: "2-digit"
+                                        }
+                                    )}
+                                </p>
+                                <p>
+                                    <em>{myPost.body}</em>
+                                </p>
+                            </div>
+                        </div>
+                    );
+                })}
             </div>
             <textarea
                 rows="2"
                 cols="50"
                 placeholder="Type something and enter..."
                 onChange={onChange}
-                onKeyPress={onKeyPress}
+                onKeyPress={async (event) => {
+                    if (event.key === "Enter") {
+                        event.preventDefault(); // Prevent the default behavior of the Enter key
+                        setCurrentInput("");
+
+                        await axios
+                            .post(
+                                "http://localhost:" + config.PORT + "/newpost",
+                                {
+                                    authorID: currentUser.id, // Replace with the actual author ID
+                                    body: event.target.value,
+                                    threadID: activeTopic.id
+                                }
+                            )
+                            .then(() => {
+                                alert("Posted");
+                                setForceReload(forceReload + 1);
+                            });
+                    }
+                }}
                 value={value}
             />
         </div>
